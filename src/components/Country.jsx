13b530f59e895af75e473f80a7799466647a4f6f
navyabhatia <navyabhatia10@gmail.com>
2022-01-20 16:57:58 +0530

    country details added

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,11 +4,14 @@ import { MenuItem, Select } from "@mui/material";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
-
+import Details from "./Details";
 import FormControl from "@mui/material/FormControl";
 
 const Country = () => {
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [details, setDetails] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
@@ -21,53 +24,95 @@ const Country = () => {
     fetchData();
   }, [countries]);
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  //let cd = ["navya", "bhatia", "ankit", "kumar"];
+
   return (
     <Container fluid>
-      <Box
-        sx={{
-          marginLeft: 15,
-          width: 200,
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "nowrap",
-          alignItems: "center",
-          float: "left",
-        }}
-      >
-        <FormControl sx={{ flexGrow: 1 }}>
-          <InputLabel id="label">Country</InputLabel>
-          <Select label="Country">
-            {countries.map((country) => (
-              <MenuItem value={country.name}> {country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
-      {
-        //now for duration}
-      }
-      <Box
-        sx={{
-          marginLeft: 15, //box margin fro left
-          width: 200, //box width hai yeh
-          display: "flex",
-          flexDirection: "row-reverse",
-          flexWrap: "nowrap",
-          alignItems: "center",
-          float: "left",
-        }}
-      >
-        <FormControl sx={{ flexGrow: 1 }}>
-          <InputLabel id="label2">Duration</InputLabel>
-          <Select label="Duration">
-            <MenuItem value={1}>one</MenuItem>
-            <MenuItem value={2}>Two</MenuItem>
-            <MenuItem value={3}>Thee</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+      <div className="row">
+        <div classNmae="column">
+          <Box
+            sx={{
+              marginLeft: 15,
+              width: 200,
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "nowrap",
+              alignItems: "center",
+              float: "left",
+            }}
+          >
+            <FormControl sx={{ flexGrow: 1 }}>
+              <InputLabel id="label">Country</InputLabel>
+              <Select label="Country" onChange={handleSearch} value={search}>
+                {countries.map((country, index) => (
+                  <MenuItem value={index} onClick={() => setDetails(country)}>
+                    {" "}
+                    {country.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+        {
+          //now for duration}
+        }
+        <div className="column">
+          <Box
+            sx={{
+              marginLeft: 15, //box margin fro left
+              width: 200, //box width hai yeh
+              display: "flex",
+              flexDirection: "row-reverse",
+              flexWrap: "nowrap",
+              alignItems: "center",
+              float: "left",
+            }}
+          >
+            <FormControl sx={{ flexGrow: 1 }}>
+              <InputLabel id="label2">Duration</InputLabel>
+              <Select label="Duration">
+                <MenuItem value={1}>one</MenuItem>
+                <MenuItem value={2}>Two</MenuItem>
+                <MenuItem value={3}>Thee</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+      </div>
+
+      <div>
+        {details.length === 0 ? (
+          <h1>Select Country and Duration</h1>
+        ) : (
+          <Container>
+            <h1>Welcome to {details.name}</h1>
+
+            <h3>{details.currency}</h3>
+
+            <h4> {details.unicodeFlag} </h4>
+
+            <img
+              src={details.flag}
+              height="auto"
+              width="320px"
+              alt="country flag"
+            />
+          </Container>
+        )}
+      </div>
     </Container>
   );
 };
 
 export default Country;
+/*
+ 
+            {console.log(search)}
+            <p> {search && cd[search]} </p>
+
+*/
