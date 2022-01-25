565c973d7ff4bf0ae9bf7b4eb124add3b6c03e47
navyabhatia <navyabhatia10@gmail.com>
2022-01-25 17:49:14 +0530

    done

---
+++ src/components/Details.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Details = (detail) => {
+  return (
+    <div>
+      {detail.length === 0 ? (
+        " "
+      ) : (
+        <Container>
+          <h1>Welcome to {detail.name}</h1>
+          <Col>
+            <img
+              src={detail.flag}
+              height="auto"
+              width="320px"
+              alt="country flag"
+            />
+          </Col>
+        </Container>
+      )}
+    </div>
+  );
+};
+
+export default Details;
